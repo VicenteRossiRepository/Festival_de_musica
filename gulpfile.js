f15f14d7c1f5fabdfee9a61c9f325c7cf7f3869d
Vicente <vicente.rossi.contacto@gmail.com>
2024-12-20 20:34:04 -0300

    Crear seccion de footer y dar estilo con _footer.scss

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ import gulpSass from 'gulp-sass'
 const sass = gulpSass(dartSass)
 
 export function css( done ) {
-    src('src/scss/app.scss')
+    src('src/scss/app.scss', {sourcedmaps: true})
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('dist/css'))
+        .pipe(dest('dist/css', {sourcemaps: true}))
 
     done()
 }
