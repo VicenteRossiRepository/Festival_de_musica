076aa6f1d03d21c3b6768d375e02cb4d75901a9a
Vicente <vicente.rossi.contacto@gmail.com>
2024-12-20 21:32:03 -0300

    Agregar modal que permita mostrar imagen al dar click en galeria con app.js

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,31 @@ function crearGaleria(){
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = `Imagen Galeria`
 
+        //event handler
+
+        imagen.onclick = function(){
+           mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen)
         
     }
+}
+
+function mostrarImagen(i){
+    //generar modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.onclick = cerrarModal;
+
+    //Agreagr al HTML
+    const body = document.querySelector('body');
+    body.appendChild(modal);
+
+    console.log(modal);
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal');
+    modal?.remove();
 }
