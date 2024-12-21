81da02644ab8a49561a5e6584ad5fdf2270ad352
Vicente <vicente.rossi.contacto@gmail.com>
2024-12-20 21:49:49 -0300

    Agregar imagen al modal y clases overflow-hidden y fade-out para evitar scroll y animaciones al dar click

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,42 +1,56 @@
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function () {
     crearGaleria()
 })
 
-function crearGaleria(){
+function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     const cantidad_imagenes = 16;
 
-    for(let i = 1; i <= cantidad_imagenes; i++) {
+    for (let i = 1; i <= cantidad_imagenes; i++) {
         const imagen = document.createElement('IMG')
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = `Imagen Galeria`
 
         //event handler
 
-        imagen.onclick = function(){
-           mostrarImagen(i);
+        imagen.onclick = function () {
+            mostrarImagen(i);
         }
 
         galeria.appendChild(imagen)
-        
+
     }
 }
 
-function mostrarImagen(i){
+function mostrarImagen(i) {
+
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = `Imagen Galeria`
+
+
     //generar modal
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
     modal.onclick = cerrarModal;
+    modal.appendChild(imagen);
 
     //Agreagr al HTML
     const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
     body.appendChild(modal);
-
-    console.log(modal);
 }
 
-function cerrarModal(){
+function cerrarModal() {
     const modal = document.querySelector('.modal');
-    modal?.remove();
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500)
+
 }
